cb162674d0d4c27d4267a9a5de7c9a8dc9498624
Vinicius-de-Morais <83669073+Vinicius-de-Morais@users.noreply.github.com>
2022-05-30 15:27:03 -0300

    Updating dotman

--- dotman.sh
+++ dotman.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
-create_files_links(){
+create_files_links() {
     echo -e "\n\n Now let set up this"
-    read -p 'Do you want to update apt-get? y/n' -r ANSWER
+    read -p 'Do you want to install some apps? y/n' -r ANSWER
     echo -e "$ANSWER"
     if [ "$ANSWER" = "y" ]; then
         apt-get update
+        sudo apt install zsh
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        y
+        git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
+        source ~/.zsh-nvm/zsh-nvm.plugin.zsh
+
+        rm -rf .zshrc
+        rm -rf .gitconfig
         cd ~/.dotfiles
         ln -s ~/.dotfiles/.zshrc ~/.zshrc
         ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+        cd
+        source .zshrc
+        source .gitconfig
+        
+        goodbye
+
+    elif [ "$ANSWER" = "n" ]; then
+        echo -e "Ok, goodbye"
+        exit 1
     else
         echo -e "something wrong"
     fi
