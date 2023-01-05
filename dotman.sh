99ab468dffb7e9a95b09a8b37c94d42a41b3881d
Vinicius-de-Morais <83669073+Vinicius-de-Morais@users.noreply.github.com>
2023-01-05 14:32:04 -0300

    fix dotman to work correctly

--- dotman.sh
+++ dotman.sh
@@ -7,11 +7,15 @@ create_files_links() {
     if [ "$ANSWER" = "y" ]; then
         apt-get update
         sudo apt install zsh
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        y
+        sh -c -y "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        
         git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
         source ~/.zsh-nvm/zsh-nvm.plugin.zsh
+        
+        git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+        source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+        cd
         rm -rf .zshrc
         rm -rf .gitconfig
         cd ~/.dotfiles
